5e90c6e0984e7cf0dfe9c50f410265dbb166b719
Valartamizh <valarthamizhrr@gmail.com>
2026-02-09 20:57:18 +0530

    Solved on 9/2/26

---
+++ Medium-Problems/lengthOfLongestSubstring.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length() == 1) return 1;
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0;
+        int maxLen = 0;
+        for (int right = 0; right < s.length(); right++) {
+            char ch = s.charAt(right);
+            if (map.containsKey(ch)) {
+                left = Math.max(left, map.get(ch) + 1);
+            }
+            map.put(ch, right);
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
+}
