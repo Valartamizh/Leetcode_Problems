5398cecad15827fba454ad8ef75f41bef9ed81b4
VALARTAMIL R <161398634+Valartamizh@users.noreply.github.com>
2026-06-10 10:05:43 +0530

    Time: 0 ms (100%), Space: 43.1 MB (50.4%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,10 +1,13 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] arr=s.split(" ");
-        int temp=0;
-        for(String ch:arr){
-            temp=ch.length();
+        s=s.trim();
+        int count=0;
+        for(int i=s.length()-1;i>=0;i--){
+            char ch=s.charAt(i);
+            if(ch!=' ')count++;
+            if(ch==' ')break;
         }
-        return temp;
+        return count;
     }
+
 }
