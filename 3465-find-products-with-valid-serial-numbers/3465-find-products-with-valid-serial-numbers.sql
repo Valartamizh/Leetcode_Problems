c961cbc4e246b41e48ce061453f90bf009e5cef9
VALARTAMIL R <161398634+Valartamizh@users.noreply.github.com>
2026-06-16 09:48:25 +0530

    Time: 565 ms (10.99%), Space: 0B (100%) - LeetHub

--- 3465-find-products-with-valid-serial-numbers/3465-find-products-with-valid-serial-numbers.sql
+++ 3465-find-products-with-valid-serial-numbers/3465-find-products-with-valid-serial-numbers.sql
@@ -1,9 +1,5 @@
 # Write your MySQL query statement below
-SELECT *
+SELECT product_id, product_name, description
 FROM products
-WHERE REGEXP_LIKE(
-    description,
-    '\\bSN[0-9]{4}-[0-9]{4}\\b',
-    'c'
-)
-ORDER BY product_id;
+WHERE CAST(CONCAT(' ',description,' ') AS BINARY) REGEXP BINARY '(^| )SN[0-9]{4}-[0-9]{4}( |$)'
+ORDER BY product_id
