1536d67869ae222fa36dbc175108d328bc8ff674
VALARTAMIL R <161398634+Valartamizh@users.noreply.github.com>
2026-06-22 09:36:09 +0530

    Time: 1 ms (100%), Space: 43.2 MB (87.41%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,33 +1,18 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        int ans = Integer.MAX_VALUE;
-        Map<Character, Integer> mp = new TreeMap<>();
-
-        for (char ch : text.toCharArray()) {
-            mp.put(ch, mp.getOrDefault(ch, 0) + 1);
-        }
-
-        for (Map.Entry<Character, Integer> entry : mp.entrySet()) {
-            char ch = entry.getKey();
-            int freq = entry.getValue();
-
-            if (ch == 'a') {
-                ans = Math.min(ans, freq);
-            }
-            if (ch == 'b' && ans != Integer.MAX_VALUE) {
-                ans = Math.min(ans, freq);
-            }
-            if (ch == 'l' && ans != Integer.MAX_VALUE) {
-                ans = Math.min(ans, freq / 2);
-            }
-            if (ch == 'n' && ans != Integer.MAX_VALUE) {
-                ans = Math.min(ans, freq);
-            }
-            if (ch == 'o' && ans != Integer.MAX_VALUE) {
-                ans = Math.min(ans, freq / 2);
+        int b = 0, a = 0, l = 0, o = 0, n = 0;
+        
+        for (char c : text.toCharArray()) {
+            switch (c) {
+                case 'b': b++; break;
+                case 'a': a++; break;
+                case 'l': l++; break;
+                case 'o': o++; break;
+                case 'n': n++; break;
             }
         }
-
-        return ans != Integer.MAX_VALUE ? ans : 0;
+        l = l / 2;
+        o = o / 2;
+        return Math.min(b, Math.min(a, Math.min(l, Math.min(o, n))));
     }
 }
