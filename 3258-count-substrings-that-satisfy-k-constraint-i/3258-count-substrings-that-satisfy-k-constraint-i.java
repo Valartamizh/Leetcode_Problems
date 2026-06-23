08b3851388a3101dd76a6d303b5f3eff091351b9
VALARTAMIL R <161398634+Valartamizh@users.noreply.github.com>
2026-06-23 15:51:53 +0530

    Time: 1 ms (100%), Space: 43.2 MB (95.02%) - LeetHub

--- 3258-count-substrings-that-satisfy-k-constraint-i/3258-count-substrings-that-satisfy-k-constraint-i.java
+++ 3258-count-substrings-that-satisfy-k-constraint-i/3258-count-substrings-that-satisfy-k-constraint-i.java
@@ -1,17 +1,30 @@
 class Solution {
     public int countKConstraintSubstrings(String s, int k) {
-        int ans = 0;
-        for(int i=0;i<s.length();i++){
-            int zero=0, one = 0;
-            for(int j=i;j<s.length();j++){
-                if(s.charAt(j) == '0'){
-                    zero++;
-                }
-                else one++;
-
-                if(one <= k || zero<= k) ans++;
+        int st=0;
+        int zero=0;
+        int ones=0;
+        int ans=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='0')
+            {
+                zero++;
             }
-            
+            else{
+                ones++;
+            }
+            while(zero>k&&ones>k)
+        {
+            if(s.charAt(st)=='0')
+            {
+                zero--;
+            }
+            else{
+                ones--;
+            }
+            st++;
+        }
+        ans+=i-st+1;
         }
         return ans;
     }
