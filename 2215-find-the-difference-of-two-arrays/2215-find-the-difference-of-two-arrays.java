c93007fd7579492f501b17799c4b97430ef7143a
VALARTAMIL R <161398634+Valartamizh@users.noreply.github.com>
2026-07-11 09:57:14 +0530

    Time: 2 ms (100%), Space: 47.2 MB (85.81%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
@@ -1,33 +1,33 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-        Set<Integer> s1 = new HashSet<>();
-        Set<Integer> s2 = new HashSet<>();
-        List<List<Integer>> ans = new ArrayList<>();
-        ans.add(new ArrayList<>());
-        ans.add(new ArrayList<>());
+        int offset = 1000;
 
-        for (int i : nums1) {
-            s1.add(i);
-        }
-        
-        for (int i : nums2) {
-            s2.add(i);
+        List<Integer> res1 = new ArrayList<>();
+        List<Integer> res2 = new ArrayList<>();
+
+        boolean[] lookup1 = new boolean[2001];
+        boolean[] lookup2 = new boolean[2001];
+
+        for (int val : nums1) {
+            lookup1[offset + val] = true;
         }
 
-        for (int i : s1) {
-            if (!s2.contains(i)) {
-                ans.get(0).add(i);
+        for (int val : nums2) {
+            if (!lookup1[offset + val]) {
+                res2.add(val);
+                lookup1[offset + val] = true;
             }
+            lookup2[offset + val] = true;
         }
 
-        for (int i : s2) {
-            if (!s1.contains(i)) {
-                ans.get(1).add(i);
+        for (int val : nums1) {
+            if (!lookup2[offset + val]) {
+                res1.add(val);
+                lookup2[offset + val] = true;
             }
+            lookup1[offset + val] = true;
         }
 
-        return ans;
+        return Arrays.asList(res1, res2);
     }
 }
