118a0e4900b3a75d878f4e60498a47ca4563ecbf
VALARTAMIL R <161398634+Valartamizh@users.noreply.github.com>
2026-07-21 14:13:25 +0530

    Time: 0 ms (100%), Space: 9.1 MB (5.59%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.c
@@ -0,0 +1,9 @@
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if(p==NULL && q==NULL){
+        return 1;
+    }
+    if(p==NULL || q==NULL){
+        return 0;
+    }
+    return(p->val==q->val)&& isSameTree(p->left, q->left)&&isSameTree(p->right,q->right);
+}
