6ce688ae94e0bc04d0d1590524ef97ffcb041f99
VALARTAMIL R <161398634+Valartamizh@users.noreply.github.com>
2026-08-22 11:21:43 +0530

    Time: 1 ms (25.54%), Space: 42.5 MB (42.42%) - LeetHub

--- 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.java
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.java
@@ -1,20 +1,22 @@
 class Solution {
     public boolean checkDivisibility(int n) {
-        int t = n;
-        ArrayList<Integer> arr = new ArrayList<>();
-        while(n!=0){
-            int temp = n%10;
-            arr.add(temp);
+        int sum=0;
+        int pro=1;
+        int original=n;
+        while(n!=0)
+        {
+            int last=n%10;
+            sum=sum+last;
+            pro=pro*last;
             n/=10;
         }
-        int sum = 0;
-        int prod = 1;
-        for(int i:arr){
-            sum+=i;
-            prod*=i;
+
+        int result=sum+pro;
+        if(original%result==0)
+        {
+            return true;
         }
-        int res = sum+prod;
-        System.out.print(res);
-        return (t%res==0 ? true:false);
+
+        return false;
     }
 }
