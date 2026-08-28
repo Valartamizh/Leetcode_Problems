2d72c429130672fd25238b3d9ec9aaaacc5d8dd6
VALARTAMIL R <161398634+Valartamizh@users.noreply.github.com>
2026-08-28 12:00:47 +0530

    Time: 5 ms (86.11%), Space: 46.8 MB (47.22%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -1,30 +1,20 @@
 class Solution {
     public int[][] diagonalSort(int[][] mat) {
-      int n=mat.length;
-        int count=0;
-        int m=mat[0].length;
-        int i=0,k=0;
-         while(count<m+n){
-             if(i==n-1&&k<m){++k;}
-             if(i!=n-1){i++;}
-             
-             pass(mat,n-1-i,k,n,m);count++;
-         }
-        return mat;
-    }
-    public void pass(int [][]a,int row,int col,int n,int m){
-        int []arr=new int [101];
-        int i=row,j=col;
-        while(row<n&&col<m){
-            arr[a[row++][col++]]++;
-        }    
-        for(int k=0;k<101;k++){
-            if(arr[k]>0){
-                while(arr[k]!=0){
-                a[i++][j++]=k;
-                 --arr[k];
+        int temp = 0;
+        boolean hasChanged = true;
+        while(hasChanged){
+            hasChanged = false; 
+            for(int i = 1; i < mat.length; i++){
+                for (int j = mat[0].length-1; j > 0; j--){
+                    if(mat[i][j] < mat[i-1][j-1]){
+                        hasChanged = true;
+                        temp = mat[i][j];
+                        mat[i][j] = mat[i-1][j-1];
+                        mat[i-1][j-1] = temp;
+                    }
                 }
             }
-        } 
+        }
+        return mat;
     }
 }
